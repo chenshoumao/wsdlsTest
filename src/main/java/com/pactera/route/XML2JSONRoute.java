e54e98b71075c56632f00986d6f10b9828b330c1
mengguoqiang <925890667@qq.com>
2019-11-01 15:22:43 +0800

    first commit

---
+++ src/main/java/com/pactera/route/XML2JSONRoute.java
@@ -0,0 +1,31 @@
+package com.pactera.route;
+
+import com.pactera.proccess.CamelProccess;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xmljson.XmlJsonDataFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author simonMeng
+ * @version 1.0
+ * @date 2019/10/16
+ **/
+@Component
+public class XML2JSONRoute extends RouteBuilder {
+    @Autowired
+    private XmlJsonDataFormat xmlJsonFormat;
+    @Override
+    public void configure() throws Exception {
+        // xml to json数据格式的转换
+        from("direct:marshalEmployeexml2json")
+                .marshal().xmljson()
+                .to("bean:myDataformat?method=process")
+                .to("log:?level=INFO&showBody=true");
+
+        // json to xml数据格式转换
+        from("direct:unMarshalEmployeejson2xml")
+                .unmarshal(xmlJsonFormat)
+                .to("log:?level=INFO&showBody=true");
+    }
+}
